16cbd0cfc834b85da3fb0f98d5b9b4af6379fa1c
Endre-Laszlo ALBERT <aendre@bitdefender.com>
2023-03-26 21:45:35 +0300

    building windows and stufF

--- main.py
+++ main.py
@@ -1,31 +1,58 @@
 import logging
+import pymem
+from tkinter import *
+
+CSGO_PROCESS_NAME = "csgo.exe"
 
 class Cheat:
     def __init__(self):
         self.logger = logging.getLogger("Cheat")
 
-    def connect_to_process(self, pid: int):
-        self.logger.info("Connecting to process with PID %s", pid)
+    def connect_to_process(self, process_name: str):
+        self.logger.info("Connecting to process  %s", process_name)
+        self.process = pymem.Pymem(process_name)
         # implementation
 
     def disconnect_from_process(self):
         self.logger.info("Disconnecting from process")
         # implementation
 
-    def findProcessByName(self, name: str):
-        self.logger.info("Searching for process by name %s", name)
-        # implementation
-
     def serve(self):
         self.logger.info("Starting to serve")
         # implementation
 
+def build_window():
+    window = Tk()
+
+    window.title("Cheat boss")
+    window.configure(width=500, height=300)
+    window.configure(bg='lightgray')
+    window.geometry("500x500")
+    l = Label(window, text = "CSGO cheat")
+    l.config(font =("Courier", 14))
+    l.pack()
+
+    button1_state = BooleanVar()
+    button2_state = BooleanVar()
+    button3_state = BooleanVar()
+
+    button1 = Checkbutton(window, text="ESP", variable=button1_state)
+    button2 = Checkbutton(window, text="Aim bot", variable=button2_state)
+    button3 = Checkbutton(window, text="Trigger bot", variable=button3_state)
+
+    # Add the buttons to the window
+    button1.pack()
+    button2.pack()
+    button3.pack()
+
+    window.mainloop()
+
+
 def main():
-    logging.basicConfig(level=logging.INFO)
+    build_window()
     cheat = Cheat()
-    process_id = cheat.findProcessByName("csgo.exe")
-    cheat.connect_to_process(process_id)
-    cheat.serve()
+    cheat.connect_to_process(CSGO_PROCESS_NAME)
+    
 
 if __name__ == "__main__":
     main()
