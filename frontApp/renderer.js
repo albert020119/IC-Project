fa137103f6f5296ca465e348f207ef5c6e40da0a
Tudor Androne <tudor.androne@student.upt.ro>
2023-04-04 14:33:40 +0300

    added functionality to each checkbox and made some styling

--- frontApp/renderer.js
+++ frontApp/renderer.js
@@ -1,8 +1,43 @@
 const { ipcRenderer } = require('electron');
 
-const myCheckbox = document.querySelector('#my-checkbox');
+const checkboxAttach = document.querySelector('#checkboxAttach');
+const checkboxAim = document.querySelector('#checkboxAim');
+const checkboxWall = document.querySelector('#checkboxWall');
+const checkboxRadar = document.querySelector('#checkboxRadar');
+const checkboxAntiflash= document.querySelector('#checkboxAntiflash');
+const checkboxTrigger= document.querySelector('#checkboxTrigger');
 
-myCheckbox.addEventListener('change', (event) => {
+
+checkboxAttach.addEventListener('change', (event) => {
+  if (event.target.checked) {
+    ipcRenderer.send('run-python-script');
+  }
+  checkbox.blur(); // remove focus from the checkbox
+  setTimeout(function() {
+    checkbox.focus(); // add focus back to the checkbox
+  }, 0);
+});
+checkboxAim.addEventListener('change', (event) => {
+  if (event.target.checked) {
+    ipcRenderer.send('run-python-script');
+  }
+});
+checkboxWall.addEventListener('change', (event) => {
+  if (event.target.checked) {
+    ipcRenderer.send('run-python-script');
+  }
+});
+checkboxRadar.addEventListener('change', (event) => {
+  if (event.target.checked) {
+    ipcRenderer.send('run-python-script');
+  }
+});
+checkboxAntiflash.addEventListener('change', (event) => {
+  if (event.target.checked) {
+    ipcRenderer.send('run-python-script');
+  }
+});
+checkboxTrigger.addEventListener('change', (event) => {
   if (event.target.checked) {
     ipcRenderer.send('run-python-script');
   }
